282072df3d37c91ed9d37f832404252fe9eedc03
NataliShatina <natali.shatina@gmail.com>
2026-06-29 18:25:06 +0000

    Deploying to gh-pages from @ NataliShatina/goit-js-blended-6@83d37bc655f27956541811d97c6c894963da5bf3 🚀

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import"./assets/styles-JE8YjOlG.js";import{a as e}from"./assets/vendor-N5iQpiFS.js";const l="https://dummyjson.com",r={categories:"products/category-list",pages:"products"},o=12;e.defaults.baseURL=l;const g=async()=>(await e.get(r.categories)).data,_=async t=>(await e.get(r.pages,{params:{limit:o,skip:(t-1)*o}})).data,a={categories:document.querySelector(".categories"),products:document.querySelector(".products")},m=t=>{const s=t.map(c=>`<li class="categories__item">
-   <button class="categories__btn" type="button">${c}</button>
+import"./assets/styles-JE8YjOlG.js";import{a as r}from"./assets/vendor-N5iQpiFS.js";const _="https://dummyjson.com",n={categories:"/products/category-list",pages:"/products",productsByCategory:"/products/category"},i=12;r.defaults.baseURL=_;const y=async()=>(await r.get(n.categories)).data,d=async t=>(await r.get(n.pages,{params:{limit:i,skip:(t-1)*i}})).data,m=async t=>(await r.get(`${n.productsByCategory}/${t}`)).data,o={categories:document.querySelector(".categories"),products:document.querySelector(".products")},b=t=>{const s=t.map(e=>`<li class="categories__item">
+   <button class="categories__btn" type="button">${e}</button>
  </li>
-`).join("");a.categories.innerHTML=s},y=t=>{const s=t.map(({title:c,brand:n,category:i,price:p,id:d,thumbnail:u})=>`<li class="products__item" data-id="${d}">
-    <img class="products__image" src="${u}" alt="${c}"/>
-    <p class="products__title">${c}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand:</span>${n}</p>
-    <p class="products__category">Category: ${i}</p>
-    <p class="products__price">Price: $${p}</p>
+`).join("");o.categories.innerHTML=s},l=t=>{const s=t.map(({title:e,brand:c,category:a,price:u,id:p,thumbnail:g})=>`<li class="products__item" data-id="${p}">
+    <img class="products__image" src="${g}" alt="${e}"/>
+    <p class="products__title">${e}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:</span>${c}</p>
+    <p class="products__category">Category: ${a}</p>
+    <p class="products__price">Price: $${u}</p>
  </li>
-`).join("");a.products.insertAdjacentHTML("beforeend",s)},b=()=>{const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")},$=async()=>{try{const t=await g();m(["all",...t]),b()}catch(t){console.log(t)}},f=async()=>{try{const t=await _(1);y(t.products)}catch(t){console.log(t)}};$();f();
+`).join("");o.products.insertAdjacentHTML("beforeend",s)},f=()=>{o.products.innerHTML=""},P=()=>{const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")},$=async()=>{try{const t=await y();b(["all",...t]),P()}catch(t){console.log(t)}},C=async()=>{try{const t=await d(1);l(t.products)}catch(t){console.log(t)}},B=async t=>{if(t.target.nodeName!=="BUTTON")return;let s=null;const e=t.target.textContent;try{e==="all"?s=(await d(1)).products:s=(await m(e)).products,f(),l(s);const c=document.querySelector(".categories__btn--active");c&&c.classList.remove("categories__btn--active"),t.target.classList.add("categories__btn--active")}catch(c){console.log(c)}};$();C();o.categories.addEventListener("click",B);
 //# sourceMappingURL=index.js.map
